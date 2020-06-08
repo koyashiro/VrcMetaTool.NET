b0bdbe3451e55eb4916d938066bfd9b46be59f6b
Koyashiro Kohaku <develop@koyashiro.net>
2020-06-09 03:04:10 +0900

    add submodule of PngChunkUtil

--- src/VrcMetaTool.NET/VrcMetaDataWriter.cs
+++ src/VrcMetaTool.NET/VrcMetaDataWriter.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
-using KoyashiroKohaku.PngMetaDataUtil;
+using KoyashiroKohaku.PngChunkUtil;
 
 namespace KoyashiroKohaku.VrcMetaTool
 {
