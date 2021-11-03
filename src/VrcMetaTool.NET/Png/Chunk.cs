04480dd64fb1a5eda0096ab79002ec6d96388daa
koyashiro <develop@koyashi.ro>
2021-11-04 00:00:08 +0900

    refactor: implement Chunk

--- src/VrcMetaTool.NET/Png/Chunk.cs
+++ src/VrcMetaTool.NET/Png/Chunk.cs
@@ -2,15 +2,20 @@ using System;
 using System.Buffers.Binary;
 using System.Text;
 
-using Force.Crc32;
-
-namespace KoyashiroKohaku.PngChunkUtil
+namespace KoyashiroKohaku.VrcMetaTool.Png
 {
     /// <summary>
     /// Chunk
     /// </summary>
-    public struct Chunk
+    public struct Chunk : IEquatable<Chunk>
     {
+        private static readonly Range LENGTH_RANGE = 0..4;
+        private static readonly Range CHUNK_TYPE_RANGE = 4..8;
+        private static readonly Range CHUNK_DATA_RANGE = 8..^4;
+        private static readonly Range CRC_TARGET_RANGE = 4..^4;
+        private static readonly Range CRC_RANGE = ^4..;
+
+
         private byte[] _buffer;
 
         public Chunk(ReadOnlySpan<byte> buffer)
@@ -33,10 +38,10 @@ namespace KoyashiroKohaku.PngChunkUtil
             _buffer = new byte[12 + chunkData.Length];
 
             var buffer = _buffer.AsSpan();
-            BinaryPrimitives.WriteInt32BigEndian(buffer.Slice(0, 4), chunkData.Length);
-            chunkType.CopyTo(buffer.Slice(4, 8));
-            chunkData.CopyTo(buffer.Slice(8, (8 + chunkData.Length)));
-            BinaryPrimitives.WriteUInt32BigEndian(buffer.Slice(8 + chunkData.Length), CalculateCrc(chunkType, chunkData));
+            BinaryPrimitives.WriteInt32BigEndian(buffer[LENGTH_RANGE], chunkData.Length);
+            chunkType.CopyTo(buffer[CHUNK_TYPE_RANGE]);
+            chunkData.CopyTo(buffer[CHUNK_DATA_RANGE]);
+            BinaryPrimitives.WriteUInt32BigEndian(buffer[CRC_RANGE], Crc32.Compute(buffer[CRC_TARGET_RANGE]));
         }
 
         public Chunk(string chunkType, string chunkData)
@@ -52,17 +57,17 @@ namespace KoyashiroKohaku.PngChunkUtil
             _buffer = new byte[12 + chunkDataBytes.Length];
 
             var buffer = _buffer.AsSpan();
-            BinaryPrimitives.WriteInt32BigEndian(buffer[0..4], chunkDataBytes.Length);
-            chunkTypeBytes.CopyTo(buffer[4..8]);
-            chunkDataBytes.CopyTo(buffer[8..(8 + chunkDataBytes.Length)]);
-            BinaryPrimitives.WriteUInt32BigEndian(buffer[(8 + chunkDataBytes.Length)..], CalculateCrc(chunkType, chunkData));
+            BinaryPrimitives.WriteInt32BigEndian(buffer[LENGTH_RANGE], chunkDataBytes.Length);
+            chunkTypeBytes.CopyTo(buffer[CHUNK_TYPE_RANGE]);
+            chunkDataBytes.CopyTo(buffer[CHUNK_DATA_RANGE]);
+            BinaryPrimitives.WriteUInt32BigEndian(buffer[CRC_RANGE], Crc32.Compute(buffer[CRC_TARGET_RANGE]));
         }
 
         public ReadOnlySpan<byte> Bytes => _buffer;
-        public ReadOnlySpan<byte> LengthBytes => _buffer[0..4];
-        public ReadOnlySpan<byte> ChunkTypeBytes => _buffer[4..8];
-        public ReadOnlySpan<byte> ChunkDataBytes => _buffer[8..(8 + ChunkDataLength)];
-        public ReadOnlySpan<byte> CrcBytes => _buffer[(8 + ChunkDataLength)..];
+        public ReadOnlySpan<byte> LengthBytes => _buffer[LENGTH_RANGE];
+        public ReadOnlySpan<byte> ChunkTypeBytes => _buffer[CHUNK_TYPE_RANGE];
+        public ReadOnlySpan<byte> ChunkDataBytes => _buffer[CHUNK_DATA_RANGE];
+        public ReadOnlySpan<byte> CrcBytes => _buffer[CRC_RANGE];
 
         public int ChunkDataLength => BinaryPrimitives.ReadInt32BigEndian(LengthBytes);
         public string ChunkType => Encoding.UTF8.GetString(ChunkTypeBytes);
@@ -81,7 +86,7 @@ namespace KoyashiroKohaku.PngChunkUtil
                 return false;
             }
 
-            if (CalculateCrc(ChunkTypeBytes, ChunkDataBytes) != Crc)
+            if (Crc32.Compute(_buffer[CRC_TARGET_RANGE]) != Crc)
             {
                 return false;
             }
@@ -89,26 +94,39 @@ namespace KoyashiroKohaku.PngChunkUtil
             return true;
         }
 
-        private static uint CalculateCrc(ReadOnlySpan<byte> chunkType, ReadOnlySpan<byte> chunkData)
-        {
-            var source = new byte[chunkType.Length + chunkData.Length];
-            chunkType.CopyTo(source.AsSpan().Slice(0, 4));
-            chunkData.CopyTo(source.AsSpan().Slice(4, chunkData.Length));
-
-            return Crc32Algorithm.Compute(source);
-        }
-
-        private uint CalculateCrc(string chunkType, string chunkData = "")
-        {
-            var chunkTypeBytes = Encoding.UTF8.GetBytes(chunkType);
-            var chunkDataBytes = Encoding.UTF8.GetBytes(chunkData);
-
-            return CalculateCrc(chunkTypeBytes, chunkDataBytes);
-        }
-
         public override string ToString()
         {
             return $"{ChunkType}: {ChunkData}";
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is Chunk other)
+            {
+                return Equals(other);
+            }
+
+            return false;
+        }
+
+        public bool Equals(Chunk other)
+        {
+            return Bytes.SequenceEqual(other.Bytes);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(_buffer);
+        }
+
+        public static bool operator ==(Chunk left, Chunk right)
+        {
+            return left.Equals(right);
+        }
+
+        public static bool operator !=(Chunk left, Chunk right)
+        {
+            return !(left == right);
         }
     }
 }
